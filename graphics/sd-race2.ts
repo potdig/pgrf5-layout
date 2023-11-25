135f0e6f48a7155060adf7c43efad81b5daaf3cd
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-11-26 00:13:46 +0900

    sd-race2

---
+++ graphics/sd-race2.ts
@@ -0,0 +1,7 @@
+import SdRace2 from '~/browser/graphics/SDRace2.svelte'
+
+const app = new SdRace2({
+  target: document.getElementById('app')!,
+})
+
+export default app
---
+++ src/browser/graphics/SDRace2.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+  import { commentatorsOf } from '../store/additions'
+  import { currentRun } from '../store/speedcontrol/run'
+  import { timer } from '../store/speedcontrol/timer'
+  import Container from './lib/Container.svelte'
+  import Account from './lib/game/Account.svelte'
+  import Background from './lib/game/Background.svelte'
+  import Footer from './lib/game/Footer.svelte'
+  import Header from './lib/game/Header.svelte'
+  import InfoBox from './lib/game/InfoBox.svelte'
+  import Split from './lib/game/Split.svelte'
+  import TextValue from './lib/game/TextValue.svelte'
+  import TimeValue from './lib/game/TimeValue.svelte'
+  import Video from './lib/game/Video.svelte'
+  import { rome } from './lib/game/rome'
+
+  const width = `${1920 * 0.4}px`
+  const height = `${1440 * 0.4}px`
+
+  $: est = $currentRun?.estimate ?? ''
+  $: teams = $currentRun?.teams ?? []
+  $: time = $timer?.time
+  $: timerStatus = $timer?.state
+  $: commentators = commentatorsOf($currentRun?.externalID ?? '')
+</script>
+
+<Container>
+  <Background></Background>
+  <Header --marginX="64px"></Header>
+  <main>
+    <div id="runners">
+      {#each teams as team, index}
+        <div class="runner">
+          <Video {width} {height}></Video>
+          <InfoBox dense={true} label={rome(index + 1)}>
+            <TextValue value={team?.players[0].name}></TextValue>
+            <Account userId={team?.players[0].id}></Account>
+          </InfoBox>
+          <Split id={team?.id}></Split>
+        </div>
+      {/each}
+    </div>
+    <div id="others">
+      <div id="commentators">
+        {#each $commentators as commentator}
+          <InfoBox label="Commentator">
+            <TextValue value={commentator.name}></TextValue>
+            <Account account={commentator.social}></Account>
+          </InfoBox>
+        {/each}
+      </div>
+      <div id="times">
+        <InfoBox label="EST">
+          <TimeValue value={est}></TimeValue>
+        </InfoBox>
+        <InfoBox label="Current Time">
+          <TimeValue value={time} status={timerStatus}></TimeValue>
+        </InfoBox>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <Footer --marginX="64px"></Footer>
+  </footer>
+</Container>
+
+<style lang="scss">
+  $marginX: 64px;
+
+  main {
+    flex-grow: 1;
+    margin: 0 $marginX;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+
+  #runners {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+
+  .runner {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #others {
+    flex-grow: 1;
+    display: grid;
+    grid-template-columns: 2fr 1fr 2fr;
+    gap: 32px;
+    align-items: end;
+  }
+
+  #commentators {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    gap: 2%;
+  }
+
+  #times {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+  }
+</style>
