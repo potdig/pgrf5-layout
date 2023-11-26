86ef4813d4bbdf4e355438d544ba69a6132dc97c
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-11-26 16:16:36 +0900

    move input HTML files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+const inputDir = 'src/browser/graphics/input'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -21,14 +22,14 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        'graphics/setup': resolve(__dirname, 'graphics/setup.html'),
-        'graphics/hd-solo': resolve(__dirname, 'graphics/hd-solo.html'),
-        'graphics/sd-solo': resolve(__dirname, 'graphics/sd-solo.html'),
-        'graphics/gb-solo': resolve(__dirname, 'graphics/gb-solo.html'),
-        'graphics/hd-race2': resolve(__dirname, 'graphics/hd-race2.html'),
-        'graphics/sd-race2': resolve(__dirname, 'graphics/sd-race2.html'),
-        'graphics/hd-race4': resolve(__dirname, 'graphics/hd-race4.html'),
-        'graphics/sd-race4': resolve(__dirname, 'graphics/sd-race4.html'),
+        'graphics/setup': resolve(__dirname, `${inputDir}/setup.html`),
+        'graphics/hd-solo': resolve(__dirname, `${inputDir}/hd-solo.html`),
+        'graphics/sd-solo': resolve(__dirname, `${inputDir}/sd-solo.html`),
+        'graphics/gb-solo': resolve(__dirname, `${inputDir}/gb-solo.html`),
+        'graphics/hd-race2': resolve(__dirname, `${inputDir}/hd-race2.html`),
+        'graphics/sd-race2': resolve(__dirname, `${inputDir}/sd-race2.html`),
+        'graphics/hd-race4': resolve(__dirname, `${inputDir}/hd-race4.html`),
+        'graphics/sd-race4': resolve(__dirname, `${inputDir}/sd-race4.html`),
       },
     },
   },
