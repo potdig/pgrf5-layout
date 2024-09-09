b87f23e732f09874e365f69281b64232653c6c65
potdig <potadig.ashnerd.zino@gmail.com>
2024-09-10 01:56:29 +0900

    new background

---
+++ src/browser/graphics/lib/Stack.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { slide } from 'svelte/transition'
+
+  export let size = 10
+
+  const brickHeight = 70
+  $: stackHeight = brickHeight * (size + 1)
+  $: gradientRate = (1 / (size + 1)) * 100
+
+  console.log(size)
+  let bricks = [...Array(size)].map((_, i) => i)
+
+  const interval = Math.floor(Math.random() * 3000) + 1000
+
+  setInterval(() => {
+    bricks = [...bricks, Math.max(...bricks) + 1]
+    setTimeout(() => {
+      bricks = [...bricks.slice(1)]
+    }, 500)
+  }, interval)
+</script>
+
+<div class="stack">
+  <div
+    class="filter"
+    style="height: {stackHeight}px; background: linear-gradient(white {gradientRate}%, rgba(0, 0, 0, 0))"
+  ></div>
+  {#each bricks as brick}
+    <div class="brick" in:slide={{ duration: 500 }}></div>
+  {/each}
+</div>
+
+<style lang="scss">
+  .stack {
+    position: relative;
+    bottom: 0;
+    height: fit-content;
+  }
+
+  .filter {
+    position: absolute;
+    bottom: 0;
+    left: -2%;
+    width: 104%;
+    z-index: 1;
+  }
+
+  .brick {
+    background-color: gray;
+    width: 180px;
+    height: 60px;
+    border-radius: 12px;
+    margin-top: 10px;
+  }
+</style>
---
+++ src/browser/graphics/lib/setup/Background.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Stack from '../Stack.svelte'
+
+  const stackSizes = [...Array(12)].map(() => Math.ceil(Math.random() * 10) + 5)
+</script>
+
+<div id="bg">
+  <div>
+    {#each stackSizes as size}
+      <Stack {size} />
+    {/each}
+  </div>
+</div>
+
+<style lang="scss">
+  #bg {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    perspective: 1000px;
+    z-index: -1;
+
+    & > div {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      bottom: -80px;
+      left: -80px;
+      display: flex;
+      align-items: end;
+      gap: 10px;
+      transform: rotate3d(1, 0, 0, 30deg);
+    }
+  }
+</style>
